165044a81a72f369dde479f5c2e22d75215593d6
Mikey Kriegel <mikkrieg@gmail.com>
2021-01-11 13:43:28 -0800

    Add a stylesheet to css directory

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,9 @@ function add(number1, number2) {
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
+const result = add(number1, number2);
 
-alert(add(number1, number2));
+alert(result);
+
+
+
