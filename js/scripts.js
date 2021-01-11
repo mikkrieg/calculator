3f6fb24ed00024c54dfe385d64059204d0c5321d
Mikey Kriegel <mikkrieg@gmail.com>
2021-01-11 15:22:34 -0800

    Add co author
    Co-authored-by: Kerry Lang <klang812@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,30 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
+
+const result = multiply(number1, number2);
 
 alert(result);
 
+// function temperatureConversion(temp1) {
+//   return (temp1 * 2) + 32;
+// }
+
+// const result = temperatureConversion(0);
+// alert(result);
 
 
